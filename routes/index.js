f6d4c8d4021f92a3e4f738af1d2389524fe25c5a
Unknown <nelsontherichboy@gmail.com>
2017-05-30 22:37:25 -0600

    cambios

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,44 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'PROYECTO DE PRUEBA NELSON AMADOR' });
+
+router.get('/contacto', function(req, res, next) {
+  res.render('contacto',{"img": "/images/imagen1.jpg" });
+  });
 });
 
+router.get('/api/usuarios', function(req,res,next){
+  var usuarios = [
+    {
+      "Nombre":"Nelson",
+      "Apellido":"Amador",
+      "Identidad": "0801199214375",
+      "Correo": "nelson15amador15@gmail.com",
+      "Areas":[
+          {"Area1":"Programación"},
+          {"Area2":"Base de Datos"},
+          {"Area3":"Inteligencia de Negocios"}
+        ]
+    }
+
+              ];
+  res.json(usuarios);
+
+});
+
+router.get('/prueba',function(req,res,next){
+res.render('mensaje',{"txtmensaje":"","msg":""});
+});
+
+var mensajes = [];
+
+router.post('/prueba',function(req,res,next){
+console.log(req.body);
+mensajes.push(req.body.txtmensaje);
+var prueba = mensajes.join("Prueba");
+var msgs = {"txtmensaje": req.body.txtmensaje,"msg": prueba };
+
+res.render('mensaje',msgs);
+});
 module.exports = router;
